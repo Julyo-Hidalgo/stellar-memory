064aef424295eb5160568e67ef09ce3c99d1761a
Maria Clara <mariacsmuharem@gmail.com>
2025-08-27 12:20:58 -0300

    adição do menu e algumas modificações do css

--- app/view/js/jogo.js
+++ app/view/js/jogo.js
@@ -20,3 +20,12 @@ document.addEventListener("DOMContentLoaded", () => {
       cartasContainer.appendChild(carta);
     }
   });
+/*botao menu */
+  document.addEventListener('DOMContentLoaded', function() {
+    const menuToggleButton = document.getElementById('menu-toggle-btn');
+    const sideMenu = document.getElementById('side_menu');
+
+    menuToggleButton.addEventListener('click', function() {
+        sideMenu.classList.toggle('show');
+    });
+});
