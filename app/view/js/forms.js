ad45d69f473855b474e9df4ddc3abd7199f64ae2
ana-vitoria-cardoso <cardosoanavitoria9@gmail.com>
2025-09-03 14:27:29 -0300

    refatoração: unificação dos estilos em forms.css e reorganização das imagens

--- app/view/js/forms.js
+++ app/view/js/forms.js
@@ -1,19 +1,19 @@
- document.addEventListener('DOMContentLoaded', function() {
-            const togglePasswordButton = document.querySelector('.toggle-password');
-            const passwordInput = document.getElementById('password');
-            
-            togglePasswordButton.addEventListener('click', function() {
-                if (passwordInput.type === 'password') {
-                    passwordInput.type = 'text';
-                    togglePasswordButton.textContent = '🔒';
-                } else {
-                    passwordInput.type = 'password';
-                    togglePasswordButton.textContent = '👁️';
-                }
-            });
-            
-            document.getElementById('profile-form').addEventListener('submit', function(e) {
-                e.preventDefault();
-                alert('Alterações salvas com sucesso!');
-            });
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    const togglePasswordButton = document.querySelector('.toggle-password');
+    const passwordInput = document.getElementById('password');
+    
+    togglePasswordButton.addEventListener('click', function() {
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+            togglePasswordButton.innerHTML = '<img src="../../../../app/view/img/cadeado.png" alt="Ocultar senha" width="20">'; 
+        } else {
+            passwordInput.type = 'password';
+            togglePasswordButton.innerHTML = '<img src="../../../../app/view/img/red-eyes.png" alt="Mostrar senha" width="20">';
+        }
+    });
+    
+    document.getElementById('profile-form').addEventListener('submit', function(e) {
+        e.preventDefault();
+        alert('Alterações salvas com sucesso!');
+    });
+});
