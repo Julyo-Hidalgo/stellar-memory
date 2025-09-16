01545ff4de166fced382b572380e816001318656
Maria Clara <mariacsmuharem@gmail.com>
2025-09-16 10:16:22 -0300

    validação e comentarios

--- app/view/js/forms.js
+++ app/view/js/forms.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
+    //Espera a página carregar
+    // garantindo que o código só vai rodar depois que todo o HTML estiver carregado/pronto.
+
     const togglePasswordButton = document.querySelector('.toggle-password');
+    //seleciona o botao de olho na senha
     const passwordInput = document.getElementById('password');
     
     togglePasswordButton.addEventListener('click', function() {
-        if (passwordInput.type === 'password') {
-            passwordInput.type = 'text';
+        //é adicionado um ouvinte de clique de botao,
+        //onde espera o usuario clicar no "olho" para mostrar a senha
+
+        if (passwordInput.type === 'password') { //se campo for tipo password
+            passwordInput.type = 'text'; //muda para text, para mostrar a senha
             togglePasswordButton.innerHTML = '<img src="../../../../app/view/img/cadeado.png" alt="Ocultar senha" width="20">'; 
-        } else {
-            passwordInput.type = 'password';
+            //imagem muda para cadeado
+        } else {//caso campo seja do tipo text
+            passwordInput.type = 'password'; //muda para password
             togglePasswordButton.innerHTML = '<img src="../../../../app/view/img/red-eyes.png" alt="Mostrar senha" width="20">';
+            //muda imagem para olho
         }
     });
     
@@ -17,3 +26,46 @@ document.addEventListener('DOMContentLoaded', function() {
         alert('Alterações salvas com sucesso!');
     });
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    //novamente espera html e pag inteira carregarem
+    const form = document.querySelector('form');
+    const submitButton = form.querySelector('button[type="submit"]');
+    //seleciona o botao de enviar na pag de login ou cadastro
+
+    submitButton.addEventListener('click', function (e) {
+    //adiciona um ouvinte de clique, para quando o usuario clicsr é redirecionado para a pag certa 
+        e.preventDefault(); 
+        // Evita que o formulário seja enviado
+
+        const isLoginPage = document.body.classList.contains('login-page');
+        //verifica se o body analisado tem loginpage (pagina de login)
+        if (isLoginPage) {
+            // Se estiver na página de login, redireciona para o jogo
+            window.location.href = '../jogo/jogo.html';
+        } else {
+            // Se estiver na página de cadastro, redireciona para o login
+            window.location.href = '../login/login.html';
+        }
+    });
+});
+
+/*OU*/
+/*
+//seleciona o botao de login
+        const botaoLogin = document.querySelector('button[type="submit"]');
+
+        botaoLogin.addEventListener('click', function (e) {
+            e.preventDefault(); // evita que o formulário seja enviado
+            window.location.href = '../jogo/jogo.html'; // redireciona para o jogo
+        });
+        // pega o botão "Cadastrar"
+        const botaoCadastrar = document.querySelector('button[type="submit"]');
+
+        botaoCadastrar.addEventListener('click', function (e) {
+            e.preventDefault(); // evita o envio do formulário
+            window.location.href = '../login/login.html'; // ajuste o caminho se o login não estiver na mesma pasta
+        });
+        */
--- app/view/js/jogo.js
+++ app/view/js/jogo.js
@@ -4,25 +4,33 @@ function loadCards() {
 
   cartasContainer.innerHTML = "";
   const verso = "../../img/1.png";
+  //limpa todo o conteudo html dentro do cointainer, 
+  //assim ele pode ser reiniciado ou ter o tamanho alterado
 
   const quantitySelector = document.getElementById(
     "configuracao-tabuleiro"
   ).value;
-
+  //pega o valor selecionado pelo usuario no select configuração do tabuleiro
   let quantidadeCartas = 16;
-
   let linhas = 4;
   let colunas = 4;
 
   if (quantitySelector !== "selecione") {
     const splitQuantity = quantitySelector.split("x");
+    //divide a string como marco desse divisão sendo o x
+    //ou seja um lado fica como coluna e outro como linha 
     linhas = parseInt(splitQuantity[0]);
     colunas = parseInt(splitQuantity[1]);
+    //converte a string em inteiros
     quantidadeCartas = linhas * colunas;
+    //calcula a qntd de cartas que deve aparecer
+    //de acordo com o que o usuario seleciona
+    //multiplicando as linhas e as colunas
   }
 
   cartasContainer.style.gridTemplateColumns = `repeat(${colunas}, 1fr)`;
   cartasContainer.style.gridTemplateRows = `repeat(${linhas}, 1fr)`;
+  //cada coluna e cada linha vao ter o mesmo tamanho
 
   for (let i = 0; i < quantidadeCartas; i++) {
     const carta = document.createElement("div");
@@ -40,7 +48,12 @@ function loadCards() {
 document.addEventListener("DOMContentLoaded", () => {
   loadCards();
 });
+//espera pag e html carregadartotalmente
 
 document.getElementById("botao-jogar").addEventListener("click", () => {
   loadCards();
 });
+//adiciona um ouvinte,  clicar botao (no caso o botao jogar)
+//quando o botao "jogar" é clicado
+// (apos o usuairo selecionar as configurações/modalidade do tabuleiro), 
+//o tabuleiro muda suas confgigurações, mostrando a quantidade d cartas selecionadas
