4f70306d54a23a03c733876bd5fc772a70e6af95
Julyo Hidalgo <99442433+Julyo-Hidalgo@users.noreply.github.com>
2025-11-07 12:36:28 +0000

    Concertando alguns erros de sintaxe na classe DAO

--- app/dao/DAO.php
+++ app/dao/DAO.php
@@ -1,14 +1,22 @@
 <?php
+
+include 'config.php';
+
 class DAO {
-    private $host = DB_HOST;
-    private $db_name = DB_NAME;
-    private $username = DB_USER;
-    private $password = DB_PASSWORD;
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
     protected $connection;
 
     function __construct() {
 	   try {
-			$this->connection = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->$username, $this->password);
+      $this->host = DB_HOST;
+      $this->db_name = DB_NAME;
+      $this->username = DB_USER;
+      $this->password = DB_PASSWORD;
+
+			$this->connection = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
 			}
 		catch(PDOException $e){
 			echo "Ocorreu um erro: " . $e->getMessage();
@@ -17,4 +25,3 @@ class DAO {
 }
 
 ?>
-
